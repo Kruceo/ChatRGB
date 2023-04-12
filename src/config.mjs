c69ad5e7b30416a26801d9f99fc2f7dc04c37500
Kruceo <98596719+Kruceo@users.noreply.github.com>
2023-04-12 16:34:12 -0300

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -1,17 +1,28 @@
 import fs from 'fs'
 
-function getRoleplay(){
+function getRoleplay(name){
     if(fs.existsSync('./data/memory/roleplay.chat')){
-        return fs.readFileSync('./data/memory/roleplay.chat','utf-8')
+        return fs.readFileSync('./data/memory/roleplay.chat','utf-8').replaceAll('#USER#',name)
     }
     else{
-        fs.writeFileSync('./data/memory/roleplay.chat','fale como uma garota de anime e usando girias')
-        return 'fale como uma garota de anime usando girias'
+        fs.writeFileSync('./data/memory/roleplay.chat','me responda como uma garota de anime animada usando girias e me chamando de #USER#')
+        return 'me responda como uma garota de anime animada usando girias e me chamando de #USER#'.replaceAll('#USER#',name)
+    }
+}
+
+function getMaxTokens(){
+    if(fs.existsSync('./data/memory/maxtoken.chat')){
+        return fs.readFileSync('./data/memory/maxtoken.chat','utf-8')
+    }
+    else{
+        fs.writeFileSync('./data/memory/maxtoken.chat','255')
+        return 255
     }
 }
 
 
-export const roleplay = getRoleplay()
+export const roleplay = getRoleplay
+export const max_tokens = getMaxTokens
 
 export const setRoleplay = (text)=>{
     fs.writeFileSync('./data/memory/roleplay.chat',text)
