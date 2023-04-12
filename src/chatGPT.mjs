f13a0960b912b6aeb11ab17e5dfc51f70bbb5836
Kruceo <98596719+Kruceo@users.noreply.github.com>
2023-04-12 16:34:48 -0300

    Update chatGPT.mjs

--- src/chatGPT.mjs
+++ src/chatGPT.mjs
@@ -1,20 +1,19 @@
 import axios from "axios";
 import { cfg } from "./utils.mjs";
 import { Configuration, OpenAIApi } from "openai"
-import { roleplay } from "./config.mjs";
+import { max_tokens, roleplay } from "./config.mjs";
 
 const configuration = new Configuration({
   apiKey: cfg.APIKEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export async function chat(message) {
+export async function chat(message,user) {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        suffix:roleplay,
-        prompt: message,
-        max_tokens: 200,
-        temperature: 0.3,
+        prompt: message + ', ' + roleplay(user),
+        max_tokens: max_tokens(),
+        temperature: 0,
       });
       console.log('[SVR] token cost: '+response.data.usage.total_tokens)
      return (response.data.choices[0].text)
